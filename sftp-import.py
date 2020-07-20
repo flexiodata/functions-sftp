ca390701c3c309e2509099aa8d2399cd59894c62
Aaron Williams <aaron@flex.io>
2020-07-20 13:59:51 -0500

    added function scaffolding

---
+++ sftp-import.py
@@ -0,0 +1,11 @@
+
+# ---
+# name: sftp-import
+# deployed: true
+# title: SFTP Import
+# description: Loads data from SFTP into indices
+# ---
+
+# main function entry point
+def flex_handler(flex):
+    flex.end([[]])
